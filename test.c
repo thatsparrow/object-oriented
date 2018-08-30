4c0351ea4e40079fbe102ba7746d534e265dbece
Jann <jbenjamin001@gmail.com>
2018-08-31 02:05:28 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,19 @@
+// Program to work with fractions – C version
+
+#include <stdio.h>
+typedef struct {
+	int numerator;
+	int denominator;
+} Fraction;
+
+int main (void)
+{
+	Fraction myFract;
+
+	myFract.numerator = 1;
+	myFract.denominator = 3;
+
+	printf ("The fraction is %i/%i\n", myFract.numerator, myFract.denominator);
+
+	return 0;
+}
